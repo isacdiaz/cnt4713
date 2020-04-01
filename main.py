23db6ae4577aeabc088238907f8e8981c71398dc
isacdiaz <60161115+isacdiaz@users.noreply.github.com>
2020-04-01 13:04:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,3 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
